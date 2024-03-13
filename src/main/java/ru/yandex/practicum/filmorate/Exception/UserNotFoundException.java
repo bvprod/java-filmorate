a831fcfde5448352a12180894f36c4f53d4442e8
Ярослав Большаков <yvbv1999@yandex.ru>
2024-03-13 10:56:47 +0300

    style: added package for exceptions

--- src/main/java/ru/yandex/practicum/filmorate/Exception/UserNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/Exception/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.controller;
+package ru.yandex.practicum.filmorate.Exception;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException() {
